b61a500e6b5d3a95037d22e9cd6e912fa8145a8a
Pup Brutus <PupBrutus@users.noreply.github.com>
2024-12-02 18:27:34 -0800

    minor updates

--- Assets/BrutusTagSystem/C# Scripts/TagGroupScript.cs
+++ Assets/BrutusTagSystem/C# Scripts/TagGroupScript.cs
@@ -3,8 +3,6 @@ using UnityEngine;
 using VRC.SDKBase;
 using VRC.Udon;
 
-//Purpose: This script is attached to the TagGroup prefab and is used to manage the position and rotation of the tag group above the owner's head in the world
-
 public class TagGroupScript : UdonSharpBehaviour
 {
     void Start()
@@ -17,28 +15,40 @@ public class TagGroupScript : UdonSharpBehaviour
         VRCPlayerApi localPlayer = Networking.LocalPlayer;
         VRCPlayerApi ownerPlayer = Networking.GetOwner(gameObject);
 
-        //set the position of this gameobject as 110% of the owner's height above the owner's head
+        //set the position of this gameobject as 115% of the owner's height above the owner's head
         if (ownerPlayer != null && localPlayer != null)
         {
-            Vector3 ownerHeadPosition = ownerPlayer.GetTrackingData(VRCPlayerApi.TrackingDataType.Head).position;
-            Vector3 ownerOriginPosition = ownerPlayer.GetTrackingData(VRCPlayerApi.TrackingDataType.Origin).position;
-            Vector3 ownerHeight = ownerHeadPosition - ownerOriginPosition;
-            Vector3 newPosition = ownerHeadPosition + new Vector3(0, ownerHeight.y * 0.1f, 0);
+            Vector3 targetHeadPosition;
+            Vector3 targetOriginPosition;
+
+            if (ownerPlayer == localPlayer)
+            {
+                targetHeadPosition = localPlayer.GetTrackingData(VRCPlayerApi.TrackingDataType.Head).position;
+                targetOriginPosition = localPlayer.GetTrackingData(VRCPlayerApi.TrackingDataType.Origin).position;
+            }
+            else
+            {
+                targetHeadPosition = ownerPlayer.GetTrackingData(VRCPlayerApi.TrackingDataType.Head).position;
+                targetOriginPosition = ownerPlayer.GetTrackingData(VRCPlayerApi.TrackingDataType.Origin).position;
+            }
+
+            Vector3 targetHeight = targetHeadPosition - targetOriginPosition;
+            Vector3 newPosition = targetHeadPosition + new Vector3(0, targetHeight.y * 0.15f, 0);
+
             transform.position = newPosition;
+
         }
+        else
+        {
+            Debug.LogError("Owner or local player is null");
+        }
+
         //set the rotation of the object to always look at the local player's head if the owner of the object is not the local player
         if (ownerPlayer != null && localPlayer != null && ownerPlayer != localPlayer)
         {
             Vector3 lookAtPosition = localPlayer.GetTrackingData(VRCPlayerApi.TrackingDataType.Head).position;
             transform.LookAt(lookAtPosition);
         }
-        //if the local player is the owner, match the owner's head rotation
-        else if (ownerPlayer == localPlayer)
-        {
-            Vector3 ownerHeadPosition = ownerPlayer.GetTrackingData(VRCPlayerApi.TrackingDataType.Head).position;
-            //transform.LookAt(ownerHeadPosition);
-        }
-        //else error
         else
         {
             Debug.LogError("Owner or local player is null");
